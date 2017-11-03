03b1de9558926235d9eef1e7810dca72696efeb9
David W.H. Swenson <dwhs@hyperblazer.net>
2017-11-03 18:26:48 +0100

    add libarchive to conda install

--- devtools/prepare/DNA_flux_example.sh
+++ devtools/prepare/DNA_flux_example.sh
@@ -6,5 +6,6 @@ version=1
 mkdir $figshare_ID
 curl -Lk -o ${figshare_ID}.tgz \
     https://ndownloader.figshare.com/articles/${figshare_ID}/versions/$version
+ls -l
 tar -xzf ${figshare_ID}.tgz -C $figshare_ID
 
