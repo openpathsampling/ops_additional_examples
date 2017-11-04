763b70f5f2eef00b5b207f600916827f36ca83da
David W.H. Swenson <dwhs@hyperblazer.net>
2017-11-04 04:15:27 +0100

    Use unzip for figshare; simplify CI while testing

--- devtools/prepare/DNA_flux_example.sh
+++ devtools/prepare/DNA_flux_example.sh
@@ -4,8 +4,8 @@ figshare_ID=4508975
 version=1
 
 mkdir $figshare_ID
-curl -Lk -o ${figshare_ID}.tgz \
+curl -Lk -o ${figshare_ID}.zip \
     https://ndownloader.figshare.com/articles/${figshare_ID}/versions/$version
 ls -l
-tar -xzf ${figshare_ID}.tgz -C $figshare_ID
+unzip -d $figshare_ID ${figshare_ID}.zip
 
