1992bf5f39f67cde45d8e1be2c1afd88d1e0cbcc
David W.H. Swenson <dwhs@hyperblazer.net>
2018-11-03 14:44:20 +0100

    --all

--- devtools/prepare/transition_analysis_Abl.sh
+++ devtools/prepare/transition_analysis_Abl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 conda install --yes --quiet pytables  # doesn't come with MDTraj?
-conda update --yes --quiet all
+conda update --yes --quiet --all
 
 bash devtools/figshare_dl_extract.sh 4496795
