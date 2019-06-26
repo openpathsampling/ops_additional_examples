9883c80259e7feda503a6e4b8f43d29008f0087f
David W.H. Swenson <dwhs@hyperblazer.net>
2019-06-26 14:25:19 +0200

    Add future to the install reqs

--- devtools/conda_ops_dev_install.sh
+++ devtools/conda_ops_dev_install.sh
@@ -3,7 +3,7 @@
 # This assumes you've already installed conda. Then it installs OPS as a
 # developer install.
 
-conda install -y --quiet -c omnia openpathsampling
+conda install -y --quiet -c omnia openpathsampling future
 conda update -y --quiet --all
 conda uninstall -y openpathsampling
 git clone https://github.com/openpathsampling/openpathsampling.git
